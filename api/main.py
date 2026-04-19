988c9216a32a2a7233e39e5d50312a0e76f77809
Thinh-nguyen-03 <thinh10nguyen05@gmail.com>
2026-04-19 00:03:48 -0500

    Add render.yaml and open CORS for deployment

--- api/main.py
+++ api/main.py
@@ -23,7 +23,7 @@ app = FastAPI(title="Trilateration Lab API", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
